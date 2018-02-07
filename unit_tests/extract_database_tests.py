88eeb4a8aefc72e077ffc6224c78a407ec449ff5
ewamate <ew.matecka@gmail.com>
2018-02-07 03:21:58 +0100

    comments changes

--- unit_tests/extract_database_tests.py
+++ unit_tests/extract_database_tests.py
@@ -2,17 +2,17 @@ import unittest
 from extract_database import extract_modomics_db
 from extract_database import extract_albany_db
 
-class TestDictOps(unittest.TestCase):
+class TestExtractingDatabases(unittest.TestCase):
 
     def test_extract_modomics_db(self):
         dict = extract_modomics_db()
         self.assertEqual(len(dict.keys()), 170)
-        self.assertEqual(set(sum([[subkey for subkey in dict[key]] for key in dict.keys()], [])), 16)
+        self.assertEqual(len(set(sum([[subkey for subkey in dict[key]] for key in dict.keys()], []))), 16)
 
     def test_extract_albany_db(self):
         dict = extract_albany_db()
         self.assertEqual(len(dict.keys()), 112)
-        self.assertEqual(set(sum([[subkey for subkey in dict[key]] for key in dict.keys()], [])), 6)
+        self.assertEqual(len(set(sum([[subkey for subkey in dict[key]] for key in dict.keys()], []))), 6)
 
 
 if __name__ == '__main__':
